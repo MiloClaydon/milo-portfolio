d59b7a0a25faec4cef75ad0090a30a3f6ebd964b
MiloClaydon <miloclaydon@hotmail.com>
2026-02-07 05:19:58 +0100

    fixing scroll problem when changing from one pge to another.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -6,9 +7,20 @@ import Manufacturing from './pages/Manufacturing';
 import Contact from './pages/Contact';
 import ProjectDetail from './pages/ProjectDetail';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <Navbar />
       <div style={{ minHeight: '80vh' }}>
         <Routes>
