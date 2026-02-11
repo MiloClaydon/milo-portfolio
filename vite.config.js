3b9f716a10d0bb052607028e27ef4a30dbf818f1
MiloClaydon <miloclaydon@hotmail.com>
2026-02-11 13:02:09 +0700

    fix large chunk problem

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const parts = id.split(/node_modules[/\\]/);
+            if (parts.length > 1) {
+              return parts[1].split(/[/\\]/)[0];
+            }
+          }
+        },
+      },
+    },
+  },
 })
